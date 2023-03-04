d1cefc4d494ddf0adb128b6034b10196cfa07150
Enoch <adesanyaenoch@gmail.com>
2023-03-04 12:52:56 +0100

    Improve FavouritesScreen: displays an message when empty

--- app/src/main/java/com/enoch02/nekoscompose/ui/screens/main/FavouritesScreen.kt
+++ app/src/main/java/com/enoch02/nekoscompose/ui/screens/main/FavouritesScreen.kt
@@ -1,18 +1,25 @@
 package com.enoch02.nekoscompose.ui.screens.main
 
 import android.app.Application
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.HeartBroken
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.enoch02.nekoscompose.R
 import com.enoch02.nekoscompose.data.db.Neko
 import com.enoch02.nekoscompose.data.model.MainViewModel
 import com.enoch02.nekoscompose.data.model.MainViewModelFactory
@@ -28,37 +35,56 @@ fun FavouritesScreen(
 ) {
     val items: List<Neko> by mainViewModel.getFavourites().collectAsState(initial = emptyList())
 
-    LazyRow(
-        modifier = modifier,
-        content = {
-            items(
-                count = items.size,
-                itemContent = { index ->
-                    var showPlaceHolder by remember { mutableStateOf(true) }
-                    val neko = items[index]
-                    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
-
-                    NekoGalleryItem(
-                        context = LocalContext.current,
-                        scope = rememberCoroutineScope(),
-                        artistHref = neko.artistHref,
-                        artistName = neko.artistName,
-                        sourceUrl = neko.sourceUrl,
-                        url = neko.url,
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .width(screenWidth)
-                            .padding(8.dp)
-                            .placeholder(
-                                visible = showPlaceHolder,
-                                color = Color.Gray,
-                                shape = RoundedCornerShape(8.dp),
-                                highlight = PlaceholderHighlight.fade(),
-                            ),
-                        onLoadingComplete = { showPlaceHolder = false }
-                    )
-                }
+    if (items.isEmpty()) {
+        Column(
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = Icons.Default.HeartBroken,
+                contentDescription = null,
+                modifier = Modifier.scale(2f)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = stringResource(R.string.no_favourites),
+                textAlign = TextAlign.Center
             )
         }
-    )
+    } else {
+        LazyRow(
+            modifier = modifier,
+            content = {
+                items(
+                    count = items.size,
+                    itemContent = { index ->
+                        var showPlaceHolder by remember { mutableStateOf(true) }
+                        val neko = items[index]
+                        val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+
+                        NekoGalleryItem(
+                            context = LocalContext.current,
+                            scope = rememberCoroutineScope(),
+                            artistHref = neko.artistHref,
+                            artistName = neko.artistName,
+                            sourceUrl = neko.sourceUrl,
+                            url = neko.url,
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .width(screenWidth)
+                                .padding(8.dp)
+                                .placeholder(
+                                    visible = showPlaceHolder,
+                                    color = Color.Gray,
+                                    shape = RoundedCornerShape(8.dp),
+                                    highlight = PlaceholderHighlight.fade(),
+                                ),
+                            onLoadingComplete = { showPlaceHolder = false }
+                        )
+                    }
+                )
+            }
+        )
+    }
 }
